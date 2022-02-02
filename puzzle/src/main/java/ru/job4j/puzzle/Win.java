1d493179b244cba55dd6f335b238dde39d1174b2
svoh86 <sms-86@mail.ru>
2022-02-02 14:05:55 +0300

    Сокобан. Тестовое задание. [#75283 #258843]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,38 +3,28 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1 && (monoHorizontal(board) || monoVertical(board))) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean monoHorizontal(int[][] board) {
-        boolean rsl = false;
-        for (int[] row : board) {
-            if (row[0] == 1) {
-                for (int j = 1; j < board.length; j++) {
-                    rsl = row[j] == 1;
-                }
-                break;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        for (int i = row; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                return false;
             }
         }
-        return rsl;
+        return true;
     }
 
-    public static boolean monoVertical(int[][] board) {
-        boolean rsl = false;
-        for (int j = 0; j < board.length; j++) {
-            if (board[0][j] == 1) {
-                for (int i = 1; i < board.length; i++) {
-                    if (board[i][j] == 1) {
-                        rsl = true;
-                    }
-                }
-                break;
+    public static boolean monoVertical(int[][] board, int column) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                return false;
             }
         }
-        return rsl;
+        return true;
     }
 }
